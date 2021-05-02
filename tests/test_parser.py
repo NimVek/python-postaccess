0bd3ee9d46d79009d5d437c17c00a473fa686b20
NimVek <NimVek@users.noreply.github.com>
2021-05-02 21:30:26 +0200

    test: fix style of test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,7 +6,10 @@ from postaccess import parser
 
 @pytest.mark.parametrize(
     ("record", "expected"),
-    [("v=spf1", "v=spf1"), ("v=spf1 ip4:1.2.3.4", "v=spf1 ip4:1.2.3.4"),],
+    [
+        ("v=spf1", "v=spf1"),
+        ("v=spf1 ip4:1.2.3.4", "v=spf1 ip4:1.2.3.4"),
+    ],
 )
 def test_parser(record, expected):
     result = parser.record.parseString(record, parseAll=True)
