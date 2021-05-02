6eea7268fadb8d3e72d2dea0ac4020cf631b25e4
NimVek <NimVek@users.noreply.github.com>
2021-05-02 21:24:13 +0200

    test: add test for parser

---
+++ tests/test_parser.py
@@ -0,0 +1,13 @@
+"""Tests for `postaccess` package."""
+import pytest
+
+from postaccess import parser
+
+
+@pytest.mark.parametrize(
+    ("record", "expected"),
+    [("v=spf1", "v=spf1"), ("v=spf1 ip4:1.2.3.4", "v=spf1 ip4:1.2.3.4"),],
+)
+def test_parser(record, expected):
+    result = parser.record.parseString(record, parseAll=True)
+    assert expected == str(result[0])
